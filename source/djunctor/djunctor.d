0523449be8b3790e64dd39e632457067e97710c7
Arne Ludwig <arne.ludwig@posteo.de>
2018-03-23 15:24:20 +0100

    move `AlignmentChain` into `djunctor`

--- source/djunctor/djunctor.d
+++ source/djunctor/djunctor.d
@@ -44,6 +44,83 @@ template AlignmentContainer(R)
     }
 }
 
+/**
+    Holds a chain of local alignments that form a compound alignment. An AlignmentChain should
+    contain at least one element.
+*/
+struct AlignmentChain
+{
+    static struct LocalAlignment
+    {
+        struct Locus
+        {
+            size_t begin;
+            size_t end;
+        }
+
+        Locus contigA;
+        Locus contigB;
+        size_t numDiffs;
+    }
+
+    static struct Contig
+    {
+        size_t id;
+        size_t length;
+    }
+
+    static enum Complement
+    {
+        yes,
+        no,
+    }
+
+    Contig contigA;
+    Contig contigB;
+    Complement complement;
+    LocalAlignment[] localAlignments;
+
+    invariant
+    {
+        assert(localAlignments.length >= 1);
+    }
+
+    int opCmp(ref const AlignmentChain other) const pure nothrow
+    {
+        import std.math : sgn;
+
+        size_t idCompare = other.contigA.id - this.contigA.id;
+        if (idCompare != 0)
+            return cast(int) sgn(idCompare);
+
+        idCompare = other.contigB.id - this.contigB.id;
+        if (idCompare != 0)
+            return cast(int) sgn(idCompare);
+
+        auto thisFirstLA = this.localAlignments[0];
+        auto otherFirstLA = other.localAlignments[0];
+        size_t locusCompare = otherFirstLA.contigA.begin - thisFirstLA.contigA.begin;
+        if (locusCompare != 0)
+            return cast(int) sgn(locusCompare);
+
+        locusCompare = otherFirstLA.contigB.begin - thisFirstLA.contigB.begin;
+        if (locusCompare != 0)
+            return cast(int) sgn(locusCompare);
+
+        auto thisLastLA = this.localAlignments[$ - 1];
+        auto otherLastLA = other.localAlignments[$ - 1];
+        locusCompare = otherLastLA.contigA.end - thisLastLA.contigA.end;
+        if (locusCompare != 0)
+            return cast(int) sgn(locusCompare);
+
+        locusCompare = otherLastLA.contigB.end - thisLastLA.contigB.end;
+        if (locusCompare != 0)
+            return cast(int) sgn(locusCompare);
+
+        return 0;
+    }
+}
+
 /// Start the `djunctor` alorithm with preprocessed options.
 void runWithOptions(in ref Options options)
 {
